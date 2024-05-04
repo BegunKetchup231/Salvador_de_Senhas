cb239f5da54527a8f456bab256503302e7efdaa6
BegunKetchup231 <142554725+BegunKetchup231@users.noreply.github.com>
2024-05-04 16:07:50 -0300

    Update

--- Salvador_de_Senhas/Form2.Designer.cs
+++ Salvador_de_Senhas/Form2.Designer.cs
@@ -35,6 +35,10 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.BtnSalvar = new System.Windows.Forms.Button();
             this.BtnBuscar = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.account_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.account_email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.account_password = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridViewSaved)).BeginInit();
             this.panel2.SuspendLayout();
@@ -47,23 +51,28 @@
             this.panel1.Controls.Add(this.DataGridViewSaved);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(437, 508);
+            this.panel1.Size = new System.Drawing.Size(494, 508);
             this.panel1.TabIndex = 0;
             // 
             // DataGridViewSaved
             // 
             this.DataGridViewSaved.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridViewSaved.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.account_type,
+            this.account_email,
+            this.account_password});
             this.DataGridViewSaved.Location = new System.Drawing.Point(0, 0);
             this.DataGridViewSaved.Name = "DataGridViewSaved";
-            this.DataGridViewSaved.Size = new System.Drawing.Size(437, 508);
+            this.DataGridViewSaved.Size = new System.Drawing.Size(491, 508);
             this.DataGridViewSaved.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(583, 12);
+            this.panel2.Location = new System.Drawing.Point(512, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(298, 508);
+            this.panel2.Size = new System.Drawing.Size(241, 508);
             this.panel2.TabIndex = 1;
             // 
             // panel3
@@ -71,28 +80,27 @@
             this.panel3.Controls.Add(this.tableLayoutPanel1);
             this.panel3.Location = new System.Drawing.Point(0, 384);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(298, 124);
+            this.panel3.Size = new System.Drawing.Size(241, 124);
             this.panel3.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.BtnSalvar, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.BtnBuscar, 0, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Controls.Add(this.BtnSalvar, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(298, 124);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(241, 124);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // BtnSalvar
             // 
             this.BtnSalvar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.BtnSalvar.Location = new System.Drawing.Point(77, 3);
+            this.BtnSalvar.Location = new System.Drawing.Point(48, 3);
             this.BtnSalvar.Name = "BtnSalvar";
             this.BtnSalvar.Size = new System.Drawing.Size(144, 56);
             this.BtnSalvar.TabIndex = 0;
@@ -103,7 +111,7 @@
             // BtnBuscar
             // 
             this.BtnBuscar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.BtnBuscar.Location = new System.Drawing.Point(77, 65);
+            this.BtnBuscar.Location = new System.Drawing.Point(48, 65);
             this.BtnBuscar.Name = "BtnBuscar";
             this.BtnBuscar.Size = new System.Drawing.Size(144, 56);
             this.BtnBuscar.TabIndex = 1;
@@ -111,11 +119,34 @@
             this.BtnBuscar.UseVisualStyleBackColor = true;
             this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
             // 
+            // id
+            // 
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            this.id.Width = 50;
+            // 
+            // account_type
+            // 
+            this.account_type.HeaderText = "Tipo de Conta";
+            this.account_type.Name = "account_type";
+            // 
+            // account_email
+            // 
+            this.account_email.HeaderText = "Email";
+            this.account_email.Name = "account_email";
+            this.account_email.Width = 150;
+            // 
+            // account_password
+            // 
+            this.account_password.HeaderText = "Senha";
+            this.account_password.Name = "account_password";
+            this.account_password.Width = 150;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(893, 532);
+            this.ClientSize = new System.Drawing.Size(760, 532);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form2";
@@ -140,5 +171,9 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button BtnSalvar;
         private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn account_type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn account_email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn account_password;
     }
 }
